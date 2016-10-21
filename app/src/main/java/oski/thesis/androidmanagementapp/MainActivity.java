b6904f6170c20f0908d9085fa8eb1e5f65652dd7
Oskar <oskar.kowalski@mail.com>
2016-10-21 23:58:58 +0200

    Buttons in start activity + link to map activity

--- app/src/main/java/oski/thesis/androidmanagementapp/MainActivity.java
+++ app/src/main/java/oski/thesis/androidmanagementapp/MainActivity.java
@@ -1,32 +1,41 @@
 package oski.thesis.androidmanagementapp;
 
+import android.app.PendingIntent;
+
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-  private ListView list;
-  private ArrayAdapter<String> adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+  }
 
-    list = (ListView) findViewById(R.id.activity_main);
+  public void setupHotSpot() {
 
-    String commands[] = {"Set up a hotspot", "Connect with WiFi", "Set destination"};
+  }
 
-    ArrayList<String> commandList = new ArrayList<String>();
-    commandList.addAll(Arrays.asList(commands));
+  public void connectWithWiFi() {
 
-    adapter = new ArrayAdapter<String>(this, R.layout.list_row, commandList);
+  }
 
-    list.setAdapter(adapter);
+  public void openMapView(View view) {
+    Intent intent = new Intent(this, MapsActivity.class);
+
+    PendingIntent pendingIntent =
+        TaskStackBuilder.create(this)
+            .addNextIntentWithParentStack(intent)
+            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
+    NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+    builder.setContentIntent(pendingIntent);
+
+    startActivity(intent);
   }
 }
