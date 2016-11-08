3ccec5ca11910af19c2247751246844c9faee291
Oskar <oskar.kowalski@mail.com>
2016-11-08 12:12:03 +0100

    Final app for azimuth navigation

--- app/src/main/java/oski/thesis/androidmanagementapp/MainActivity.java
+++ app/src/main/java/oski/thesis/androidmanagementapp/MainActivity.java
@@ -1,77 +1,86 @@
 package oski.thesis.androidmanagementapp;
 
 import android.app.PendingIntent;
-import android.content.Context;
 import android.content.Intent;
-import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.NotificationCompat;
+import android.util.Log;
 import android.view.View;
-import android.view.WindowManager;
+import android.widget.TextView;
 
-import java.lang.reflect.Method;
+import java.net.InetAddress;
+import java.util.ArrayList;
+
+import oski.thesis.androidmanagementapp.arduinoutils.ArduinoWifiAPConnectionManager;
+import oski.thesis.androidmanagementapp.wifihotspotutils.ClientScanResult;
+import oski.thesis.androidmanagementapp.wifihotspotutils.FinishScanListener;
+import oski.thesis.androidmanagementapp.wifihotspotutils.WifiApManager;
 
 /**
  * Created by Oskar Kowalski on 20.10.2016.
  */
 
 public class MainActivity extends AppCompatActivity {
-
-  public WifiManager wifiManager;
-  public Context context;
+  TextView textView;
+  WifiApManager wifiApManager;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    textView = (TextView) findViewById(R.id.textView);
+    wifiApManager = new WifiApManager(this);
   }
 
-  public void setupHotSpot(View view) {
-    if (createWifiAccessPoint()) {
-      System.out.println("CONNECTED");
-    }
-    ;
-    WindowManager windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-    recreate();
+  public void openHotSpot(View view) {
+    wifiApManager.setWifiApEnabled(null, true);
   }
 
-  public void connectWithWiFi(View view) {
+  public void closeHotSpot(View view) {
+    wifiApManager.setWifiApEnabled(null, false);
+  }
 
+  public void refreshClients(View view) {
+    scan();
   }
 
   public void openMapView(View view) {
+    scan();
     Intent intent = new Intent(this, MapsActivity.class);
-
-    PendingIntent pendingIntent =
-        TaskStackBuilder.create(this)
-            .addNextIntentWithParentStack(intent)
-            .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
+    PendingIntent pendingIntent = TaskStackBuilder.create(this)
+        .addNextIntentWithParentStack(intent)
+        .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
     NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
     builder.setContentIntent(pendingIntent);
-
     startActivity(intent);
   }
 
-  private boolean createWifiAccessPoint() {
-    WifiManager wifiManager = (WifiManager) getBaseContext().getSystemService(Context.WIFI_SERVICE);
-    if (wifiManager.isWifiEnabled()) {
-      wifiManager.setWifiEnabled(false);
-    }
-    wifiManager.setWifiEnabled(false);
-    Method[] mMethods = wifiManager.getClass().getDeclaredMethods();
-    for (Method mMethod : mMethods) {
-      if (mMethod.getName().equals("setWifiApEnabled")) {
-        try {
-          mMethod.invoke(wifiManager, null, true);
-          return true;
-        } catch (Exception ex) {
+  private void scan() {
+    wifiApManager.getClientList(false, new FinishScanListener() {
+
+      @Override
+      public void onFinishScan(final ArrayList<ClientScanResult> clients) {
+
+        textView.setText("WifiApState: " + wifiApManager.getWifiApState() + "\n\n");
+        textView.append("Clients: \n");
+        for (ClientScanResult clientScanResult : clients) {
+          textView.append("####################\n");
+          textView.append("IpAddr: " + clientScanResult.getIpAddr() + "\n");
+          textView.append("Device: " + clientScanResult.getDevice() + "\n");
+          textView.append("HWAddr: " + clientScanResult.getHWAddr() + "\n");
+          textView.append("isReachable: " + clientScanResult.isReachable() + "\n");
+          if (clientScanResult.getHWAddr().equals("5c:cf:7f:8b:af:55")) {
+            try {
+              ArduinoWifiAPConnectionManager.setArduinoDeviceIP(InetAddress.getByName(clientScanResult.getIpAddr()));
+            } catch (Exception e) {
+              Log.e(this.getClass().toString(), "", e);
+            }
+          }
         }
-        break;
       }
-    }
-    return false;
+    });
   }
 }
---
+++ app/src/main/java/oski/thesis/androidmanagementapp/arduinoutils/ArduinoWifiAPConnectionManager.java
@@ -0,0 +1,48 @@
+package oski.thesis.androidmanagementapp.arduinoutils;
+
+import android.util.Log;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.io.DataOutputStream;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * Created by Oskar Kowalski on 30.10.2016.
+ */
+
+public abstract class ArduinoWifiAPConnectionManager {
+  private static InetAddress arduinoDeviceIP;
+  private static LatLng destinationPoint;
+
+  private static int LISTEN_PORT_NUMBER = 3333;
+
+  public static void sendDestinationPointToArduino() {
+    Socket socket = new Socket();
+    InetSocketAddress socketAddress = new InetSocketAddress(arduinoDeviceIP, LISTEN_PORT_NUMBER);
+    Log.d("TCP", "C: Connecting...");
+
+    try {
+      Log.d("TCP", "C: Sending: '" + destinationPoint.toString() + "'" + " to " + arduinoDeviceIP + ":" + LISTEN_PORT_NUMBER);
+      socket.connect(socketAddress);
+      Log.e("Connect", "");
+      DataOutputStream DataOut = new DataOutputStream(socket.getOutputStream());
+      DataOut.flush();
+      DataOut.writeBytes(destinationPoint.toString());
+      DataOut.flush();
+      socket.close();
+    } catch (Exception e) {
+      Log.e("Arduino connection", "", e);
+    }
+  }
+
+  public static void setArduinoDeviceIP(InetAddress arduinoDeviceIP) {
+    ArduinoWifiAPConnectionManager.arduinoDeviceIP = arduinoDeviceIP;
+  }
+
+  public static void setDestinationPoint(LatLng destinationPoint) {
+    ArduinoWifiAPConnectionManager.destinationPoint = destinationPoint;
+  }
+}
